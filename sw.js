75c5f42f64ff6a8a60f29bd2f6b6a3bf604e2dfb
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2021-12-11 21:10:06 +0000

    Caps will be the death of me

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211211.2105';
+const cacheName = 'chirpy-20211211.2109';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
