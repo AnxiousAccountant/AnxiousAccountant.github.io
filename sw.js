6e3adcee7b0b275095ecb0d188d05f00b3fefaab
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2021-12-26 21:02:13 +0000

    remove google an

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211226.2041';
+const cacheName = 'chirpy-20211226.2102';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
