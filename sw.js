449518aba7170f054edc7594eb219a972f9218c1
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2021-12-27 20:07:14 +0000

    comments activated

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211227.1855';
+const cacheName = 'chirpy-20211227.2004';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
