dfb3629fbfd22071ed5718c7659c0cb8b8e72d72
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-01-11 22:49:13 +0000

    another ticker update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220111.2238';
+const cacheName = 'chirpy-20220111.2248';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
