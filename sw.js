7f0db642eaecc45b6590ff72a3a7b762da3ccf06
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-03-30 20:59:04 +0100

    enw

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220320.1100';
+const cacheName = 'chirpy-20220330.2058';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
