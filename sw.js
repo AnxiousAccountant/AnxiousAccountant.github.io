5ecc04ea49b347d521bc569d5d0cf01713b964f7
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-04-01 20:54:46 +0100

    next1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220401.2051';
+const cacheName = 'chirpy-20220401.2054';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
