c71754c27fcc0c61fde7848e5350829d8425fc80
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-06-07 21:33:59 +0100

    lemon shandy

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220606.2223';
+const cacheName = 'chirpy-20220607.2133';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
