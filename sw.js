6a99f61e4c481f43decfd433a07c65381bdb0286
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-06-09 21:56:42 +0100

    limbo

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220607.2300';
+const cacheName = 'chirpy-20220609.2153';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
