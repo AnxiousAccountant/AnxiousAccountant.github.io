0344f138c45a246cc50f384aac923f31779d7871
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-07-02 21:41:11 +0100

    stoop2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220702.2138';
+const cacheName = 'chirpy-20220702.2140';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
