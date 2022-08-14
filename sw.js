1e9c87a1334727699ac44fc8394d2d82ebf164b9
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-08-14 16:20:11 +0100

    lazer fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220726.2050';
+const cacheName = 'chirpy-20220814.1618';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
