396d8f007f4a06e1f959de8f2b8dac1a612331ec
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-08-25 19:23:02 +0100

    brummie

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220814.1618';
+const cacheName = 'chirpy-20220825.1908';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
