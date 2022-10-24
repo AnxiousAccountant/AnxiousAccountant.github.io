5e160e5d6a8d87764e817711e9f791e2fc852e25
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-10-24 18:21:02 +0100

    spelling

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221024.1226';
+const cacheName = 'chirpy-20221024.1820';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
