5bcf8c9aabd505deee36f30dae8f51ad2e12fd0c
AndyJBoo <76838776+AndyJBoo@users.noreply.github.com>
2022-10-24 12:18:47 +0100

    imposter

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221022.1701';
+const cacheName = 'chirpy-20221024.1216';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
